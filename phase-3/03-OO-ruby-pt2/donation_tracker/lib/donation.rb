5053b197cec20ed21df86cccff3eee5ba9eecb05
marisasaunders <marisaksaunders@gmail.com>
2022-08-16 20:58:58 -0400

    finished

--- phase-3/03-OO-ruby-pt2/donation_tracker/lib/donation.rb
+++ phase-3/03-OO-ruby-pt2/donation_tracker/lib/donation.rb
@@ -1,8 +1,51 @@
-class Donation 
+class Donation
+    
+    @@all = [] #class variable: 
 
     attr_accessor :amount, :organization, :date, :completed
+    attr_reader :id
 
-    def initialize(organization, amount, date, completed)
+    def self.all # class method, it is called on the class itself
+        # self: will return the class bc/ inside scope of a class method
+        @@all
+    end
+
+    # to invoke .all => Donation.all
+
+    def self.create(attributes)
+       self.new(attributes).save     
+    end
+
+    def self.primary_key
+        # Build some logic
+        #  if its the first element, make id = 1
+        # else increment by 1
+
+        if @@all.empty?
+            1
+        else
+            @@all[-1].id + 1
+        end
+    end
+
+    def self.find_by_id(id)
+        # I want to check all of the instances and find the one that maatches the given id
+        # mapping to return an array
+        # find, or filter
+        @@all.find do |donation|
+            donation.id == id
+        end
+    end
+
+    def self.pending
+        # return an array of all donations with a completed:false
+        @@all.filter do |donation|
+            donation.completed == false
+        end
+    end
+
+    def initialize(id:, organization:, amount:, date:, completed:)
+        @id = Donation.primary_key
         @organization = organization 
         @amount = amount 
         @date = date 
@@ -14,6 +57,13 @@ class Donation
         puts "Amount: #{@amount}"
         puts "Date: #{@date}"
         puts "Completed? #{@completed}"
+    end
+
+    # create a save method, that saves each new instance
+
+    def save
+        @@all << self
+        self
     end
 
 end 
